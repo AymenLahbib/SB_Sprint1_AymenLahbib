ab3786e1ec6d0e55a175f326b8aaf1a159488600
Aymen lAHBIB <allahbib38@gmail.com>
2022-05-25 20:51:02 +0100

    SB_MiniProjet

---
+++ src/main/java/com/aymen/departements/repos/UserRepository.java
@@ -0,0 +1,7 @@
+package com.aymen.departements.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.aymen.departements.entities.User;
+public interface UserRepository extends JpaRepository<User, Long> {
+User findByUsername (String username);
+}
